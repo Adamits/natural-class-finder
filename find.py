6dedd34efbe8473cf39ab1d0fcd6c2907fa9052e
Adam <ajwieme@gmail.com>
2017-04-13 15:51:44 -0600

    making templates

---
+++ find.py
@@ -0,0 +1,13 @@
+from matrices import *
+
+def find_natural_class(phonemes=[]):
+  return True
+
+def assess_manner():
+  return True
+
+def assess_place():
+  return True
+
+def assess_voice():
+  return True
