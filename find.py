0c326586adb950c568379a646981b46181f8fb63
Adam <ajwieme@gmail.com>
2017-04-23 11:44:06 -0600

    commenting functions and adding gitignore

--- find.py
+++ find.py
@@ -1,13 +1,17 @@
 from matrices import *
 
+# main function
 def find_natural_class(phonemes=[]):
   return True
 
+# extracts necessary manner features
 def assess_manner():
   return True
 
+# extracts necessary place features
 def assess_place():
   return True
 
+# extracts necessary voicing feature
 def assess_voice():
   return True
