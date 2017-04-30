11743d1205ac2e18007b58b685f73f6f990c0510
jkirchner1206@gmail.com <jkirchner1206@gmail.com>
2017-04-30 15:07:18 -0600

    made final assess function

--- find.py
+++ find.py
@@ -106,18 +106,37 @@ def assess_vowels():
   return True
 
 # extracts necessary voicing feature
-def assess_voice():
-  return True
+def assess_voice(features):
+	shared_features = fm.get_shared_voice_features(features)
+	return shared_features
+
+
+def assess_optimal(phonemes = []):
+	optimal = []
+	manner = assess_manner(phonemes)
+	place = assess_place(phonemes)
+	voice = assess_voice(phonemes)
+	
+	optimal.extend(manner)
+	optimal.extend(place)
+	if "-delayed_release" in manner or "-continuant" in manner or "-sonorant" in manner:
+		optimal.extend(voice)
+		
+	return optimal
 
 # Demonstrate usage of FeaturesMatrix
 # May still need to implement something for unicode
 
+print assess_optimal(["t", "k", "p"])
+print assess_optimal(["w", "j"])
 
-print assess_manner(["a", "e"])	# vowels
+"""
+print assess_manner(["i", "e"])	# vowels
 print assess_manner(["w", "j"])	# glides
 print assess_manner(["m", "n"])	# nasals
 print assess_manner(["f", "s"])	# fricatives
 print assess_manner(["t", "k"])	# stops
+"""
 
 
 
